a303edf10f4f7ef0b856ab21f1883f411aac2954
Dinda Yulianita <88498468+treasurebmxx@users.noreply.github.com>
2024-09-12 22:52:56 +0700

    Add files via upload

---
+++ revou-fundamental/js/reset.js
@@ -0,0 +1,7 @@
+function showluassegitiga(flag) {
+    document.getElementById('result-luas-segitiga').style.display = flag === 1 ? 'block' : 'none';
+}
+
+function showkelilingsegitiga(flag) {
+    document.getElementById('result-keliling-segitiga').style.display = flag === 1 ? 'block' : 'none';
+}
